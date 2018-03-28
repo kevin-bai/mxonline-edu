75266f566e6b988d800e24e7a1760fbf3ea77880
kevin-bai <897840029@qq.com>
2018-03-28 16:16:26 +0800

    html中路径替换

--- mxonline/urls.py
+++ mxonline/urls.py
@@ -21,7 +21,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
-    
+
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^login/$', TemplateView.as_view(template_name='login.html'), name='login'),
+    url(r'^register/$', TemplateView.as_view(template_name='register.html'), name='register'),
 ]
